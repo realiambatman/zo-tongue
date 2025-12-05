9f0076b299be24f6a1841b1975ea959964ea2180
realiambatman <kapthianmuang@frameimpacts.com>
2025-12-05 10:53:28 +0530

    Temporarily disable Vaiphei language option
    Commented out Vaiphei from LANGUAGE_OPTIONS as the language support is not good enough yet. Can be re-enabled when quality improves.

--- constants.ts
+++ constants.ts
@@ -1,18 +1,18 @@
-import { SupportedLanguage } from './types';
+import { SupportedLanguage } from "./types";
 
 export const LANGUAGE_OPTIONS: SupportedLanguage[] = [
   SupportedLanguage.English,
   SupportedLanguage.Paite,
   SupportedLanguage.Thadou,
   SupportedLanguage.Hmar,
-  SupportedLanguage.Vaiphei,
+  // SupportedLanguage.Vaiphei, // Commented out - not good enough yet
   SupportedLanguage.Mizo,
   SupportedLanguage.Zou,
   SupportedLanguage.Kom,
   SupportedLanguage.Gangte,
 ];
 
-export const MODEL_NAME = 'gemini-3-pro-preview';
+export const MODEL_NAME = "gemini-3-pro-preview";
 
 export const INITIAL_CHAT_MESSAGE = (lang: SupportedLanguage): string => {
   return `Chibai! I am your ${lang} assistant. I will only speak in ${lang}.`;
